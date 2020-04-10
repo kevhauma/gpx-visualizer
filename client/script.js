21454b14ceb70c31efed0d5647a6e832c92f7e75
kevhauma <hauman.kevin@hotmail.com>
2020-04-10 09:59:39 +0200

    playing around with ratio and stretching

--- client/script.js
+++ client/script.js
@@ -5,13 +5,15 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 
 async function setup() {
+    
     colorMode(HSB);
     createCanvas(w, h);
+    background(255);
     let start = Date.now();
 
-    data = await (await fetch(`/data`)).json();    
-    let onlyLat = [...data.map(set => [...set.points.map(p => p.lat)])].flat()
-    let onlyLon = [...data.map(set => [...set.points.map(p => p.lon)])].flat()
+    data = await (await fetch(`/data`)).json();
+    let onlyLat = [...data.map(set => [...set.points.map(p => p.lat)])].flat();
+    let onlyLon = [...data.map(set => [...set.points.map(p => p.lon)])].flat();
 
     const minLat = Math.min(...onlyLat);
     const maxLat = Math.max(...onlyLat);
@@ -20,43 +22,53 @@ async function setup() {
     const maxLon = Math.max(...onlyLon);
     
     
+    let deltaLat = maxLat - minLat;
+    let deltaLon = maxLon - minLon;
+    
+    let coordinateRatio = deltaLat/deltaLon;
+    
+    let xScale = 1;
+    let yScale = 1;
+    
+    if(coordinateRatio > 1){
+        xScale = coordinateRatio;
+    } else{
+        yScale = coordinateRatio;
+    }
     
     data = data.map(set => {
         let newPoints = set.points.map(p => {
             let newP = {                
-                x: map(p.lon, minLon, maxLon, 0, w).toFixed(2),
-                y: map(p.lat, minLat, maxLat, h, 0).toFixed(2),
+                x: (map(p.lon, minLon, maxLon, 0, w)/xScale).toFixed(2),
+                y: (map(p.lat, minLat, maxLat, h, 0)*yScale).toFixed(2),
                 date: p.date
-            }            
-            return newP
-        })
+            }; 
+            return newP;
+        });
         return {
             name: set.name,
             points: newPoints,
             date: set.date
-        }
-    })
-    data.sort((a, b) => a.points[0].date > b.points[0].date)
+        };
+    });
+    data.sort((a, b) => a.points[0].date > b.points[0].date);
 }
 
 function mousePressed() {
-    //    noLoop()
+    
 }
 
-let p = {}
-
 function draw() {
-    if (!data) return
-    stroke(0)
-    line(0,w,0,h)
-    strokeWeight(1)
+    if (!data) return;
+    stroke(0);
+    line(0,w,0,h);
+    strokeWeight(1);
     data.forEach(set => {
-        let prevP = set.points.shift()
+        let prevP = set.points.shift();
         for (let p of set.points) {            
-            line(prevP.x, prevP.y, p.x, p.y)
-            prevP = p
+            line(prevP.x, prevP.y, p.x, p.y);
+            prevP = p;
         }
     })
-    noLoop()
-
+    noLoop();
 }
